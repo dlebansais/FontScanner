6fb36efb0582fb27c77348ae893ba02f23c0b0e6
dlebansais <dlebansais@gmail.com>
2022-09-03 18:58:35 +0200

    CI update.

--- coverage.bat
+++ coverage.bat
@@ -9,8 +9,8 @@ set TESTPROJECTNAME=Test-%PROJECTNAME%
 
 set OPENCOVER_VERSION=4.7.1221
 set OPENCOVER=OpenCover.%OPENCOVER_VERSION%
-set CODECOV_VERSION=1.13.0
-set CODECOV=Codecov.%CODECOV_VERSION%
+set CODECOV_VERSION=0.2.4
+set CODECOV=CodecovUploader.%CODECOV_VERSION%
 
 set RESULTFILENAME=Coverage-%PROJECTNAME%.xml
 set RESULTOUTPUT=".\Test\temp\%RESULTFILENAME%"
@@ -18,13 +18,13 @@ set FRAMEWORK=net48
 
 nuget install OpenCover -Version %OPENCOVER_VERSION% -OutputDirectory packages
 if not exist ".\packages\%OPENCOVER%\tools\OpenCover.Console.exe" goto error_console1
-nuget install CodeCov -Version %CODECOV_VERSION% -OutputDirectory packages
+nuget install CodecovUploader -Version %CODECOV_VERSION% -OutputDirectory packages
 if not exist ".\packages\%CODECOV%\tools\codecov.exe" goto error_console2
 
 mkdir Test\temp
 ".\packages\%OPENCOVER%\tools\OpenCover.Console.exe" -register:user -target:"runtests.bat" -filter:"+[%PROJECTNAME%*]* -[%TESTPROJECTNAME%*]*" -output:%RESULTOUTPUT%
 
-if exist %RESULTOUTPUT% .\packages\%CODECOV%\tools\win7-x86\codecov -f %RESULTOUTPUT% -t %PROJECT_TOKEN%
+if exist %RESULTOUTPUT% .\packages\%CODECOV%\tools\codecov -f %RESULTOUTPUT% -t %PROJECT_TOKEN%
 del .\Test\temp\*.xml
 rmdir Test\temp
 
